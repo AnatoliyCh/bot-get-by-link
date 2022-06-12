f7ff3ba76848cd76bd9a8eb985039d90e2a86488
Anatoly Ch <googlsocial97@gmail.com>
2022-06-12 16:02:08 +0700

    Basic logic implemented Bot.GetByLink.Client.Telegram.Polling
    Init appsettings.json
    Update README.md

--- Bot.GetByLink.Client.Telegram.Polling/Program.cs
+++ Bot.GetByLink.Client.Telegram.Polling/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using Bot.GetByLink.Client.Telegram.Polling;
+using Microsoft.Extensions.Configuration;
+
+// get project name
+var projectName = Assembly.GetExecutingAssembly().GetName().Name;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true)
+    .AddEnvironmentVariables().Build();
+
+configuration["project-name"] = projectName;
+
+var client = new ClientPolling(configuration);
+
+var startMessage = $"{projectName}: start";
+
+var launched = await client.Start();
+
+if (launched)
+{
+    Console.WriteLine(startMessage);
+    await client.SendTextMessageToLogChatAsync(startMessage);
+}
+
+var looping = true;
+
+while (looping)
+{
+    Console.Write("Command: ");
+    var command = Console.ReadLine();
+    switch (command)
+    {
+        case "exit polling":
+            looping = false;
+            client.Stop();
+            break;
+        case "start polling":
+            Console.WriteLine($"{projectName}: starting polling...");
+            await client.Start();
+            break;
+        case "stop polling":
+            Console.WriteLine($"{projectName}: stoping polling...");
+            client.Stop();
+            break;
+    }
+}
