ab7e2636b3681abeab1392dda100a4f53c6481dc
Владимир Меркель <v.merkel778@gmail.com>
2022-06-13 22:33:31 +0700

    Апи реддита для получения контекта поста по id поста

---
+++ Bot.GetByLink.Proxy.Reddit/AccessTokenReddit.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Bot.GetByLink.Proxy.Reddit
+{
+    /// <summary>
+    /// Класс токена доступа у Реддита.
+    /// </summary>
+    internal class AccessTokenReddit
+    {
+        /// <summary>
+        /// Gets or sets Токен доступа.
+        /// </summary>
+        [JsonProperty("access_token")]
+        public string? AccessToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets Тип токена.
+        /// </summary>
+        [JsonProperty("token_type")]
+        public string? TokenType { get; set; }
+
+        /// <summary>
+        /// Gets or sets Сколько секунд живёт токен.
+        /// </summary>
+        [JsonProperty("expires_in")]
+        public string? ExpiresIn { get; set; }
+
+        /// <summary>
+        /// Gets or sets Какой скоуп доступен для данного токена (* - все).
+        /// </summary>
+        [JsonProperty("scope")]
+        public string? Scope { get; set; }
+    }
+}
---
+++ Bot.GetByLink.Proxy.Reddit/ProxyReddit.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Reddit;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Bot.GetByLink.Proxy.Reddit
+{
+    /// <summary>
+    /// Апи реддита для получения поста по id.
+    /// </summary>
+    public class ProxyReddit
+    {
+        private const string UriString = "www.reddit.com";
+        private readonly string appId;
+        private readonly string secretId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProxyReddit"/> class.
+        /// Создание класса Реддита.
+        /// </summary>
+        /// <param name="appId">Id приложения реддита.</param>
+        /// <param name="secretId">Secret Id приложения реддита.</param>
+        public ProxyReddit(string appId, string secretId)
+        {
+            this.appId = appId;
+            this.secretId = secretId;
+        }
+
+        /// <summary>
+        /// Метод для получения контента поста реддита по ид поста.
+        /// </summary>
+        /// <param name="postId">Ид поста.</param>
+        /// <returns>Объект с текстом и ссылками на картинки и видео присутствующие в посте.</returns>
+        /// <exception cref="ArgumentNullException">Возвращается в случае если был передан пустой postId.</exception>
+        /// <exception cref="HttpRequestException">Возвращается если не получилось авторизоваться в реддите или он вернул пустой ответ.</exception>
+        public TelegramMessage GetPostId(string postId)
+        {
+            if (string.IsNullOrWhiteSpace(postId))
+            {
+                throw new ArgumentNullException(nameof(postId), "Пустой id поста");
+            }
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri($"https://{UriString}");
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/v1/access_token");
+
+            var byteArray = new UTF8Encoding().GetBytes($"{appId}:{secretId}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            var formData = new List<KeyValuePair<string, string>>();
+            formData.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            formData.Add(new KeyValuePair<string, string>("device_id", appId));
+
+            request.Content = new FormUrlEncodedContent(formData);
+            var response = client.SendAsync(request).GetAwaiter().GetResult();
+
+            var accsessTokenObject = JsonConvert.DeserializeObject<AccessTokenReddit>(response.Content.ReadAsStringAsync().Result);
+            response.EnsureSuccessStatusCode();
+
+            if (accsessTokenObject == null) throw new HttpRequestException($"Реддит вернул ошибку: {response.Content.ReadAsStringAsync().Result}");
+            var redditAccess = new RedditClient(appId, appSecret: secretId, accessToken: accsessTokenObject.AccessToken, userAgent: "bot-get-by-link-web");
+            var post = redditAccess.LinkPost($"t3_{postId}").Info();
+            if (post.Listing.Media == null && !post.Listing.IsVideo && !post.Listing.IsRedditMediaDomain) return new TelegramMessage() { Text = $"{post.Listing.URL}\n\n{post.Listing.SelfText}" };
+            if (Regex.IsMatch(post.Listing.URL, @"https?://\S+(?:jpg|jpeg|png)", RegexOptions.IgnoreCase)) return new TelegramMessage() { Text = post.Listing.SelfText, UrlPicture = post.Listing.URL };
+            return new TelegramMessage() { Text = post.Listing.SelfText, UrlVideo = post.Listing.URL };
+        }
+    }
+}
