1d065f3e2f4a325a27be97b81d8d5b04a24cc79a
Anatoly Ch <googlsocial97@gmail.com>
2022-06-13 17:59:30 +0700

    Add base implemented interfases and classes
    Client/IClient
    Command/ICommand

---
+++ Bot.GetByLink.Common.Infrastructure/Abstractions/Command.cs
@@ -0,0 +1,35 @@
+﻿using Bot.GetByLink.Common.Infrastructure.Interfaces;
+
+namespace Bot.GetByLink.Common.Infrastructure.Abstractions;
+
+/// <summary>
+///     Base abstract command class.
+/// </summary>
+public abstract class Command : ICommand
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Command" /> class.
+    /// </summary>
+    /// <param name="name">Command name.</param>
+    protected Command(string name)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+    }
+
+    /// <summary>
+    ///     Gets command name.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    ///     Run command.
+    /// </summary>
+    /// <param name="ctx">Context command.</param>
+    public abstract void Execute(object? ctx = null);
+
+    /// <summary>
+    ///     Rollback command.
+    /// </summary>
+    /// <param name="ctx">Context command.</param>
+    public abstract void Undo(object? ctx = null);
+}
---
+++ Bot.GetByLink.Common.Infrastructure/Interfaces/ICommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Bot.GetByLink.Common.Infrastructure.Interfaces;
+
+/// <summary>
+///     Command Interface.
+/// </summary>
+public interface ICommand
+{
+    /// <summary>
+    ///     Run command.
+    /// </summary>
+    /// <param name="ctx">Context command.</param>
+    public void Execute(object? ctx = null);
+
+    /// <summary>
+    ///     Rollback command.
+    /// </summary>
+    /// <param name="ctx">Context command.</param>
+    public void Undo(object? ctx = null);
+}
