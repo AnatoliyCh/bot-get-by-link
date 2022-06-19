2283a5529182b15e86b372b276557d5337ce1fbd
Vladimir Merkel <v.merkel778@gmail.com>
2022-06-19 16:35:31 +0700

    Create class for telegram message

---
+++ Bot.GetByLink.Common.Infrastructure/TelegramMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.GetByLink.Common.Infrastructure
+{
+    /// <summary>
+    /// Временный класс для возвратного сообщения.
+    /// </summary>
+    public class TelegramMessage
+    {
+        /// <summary>
+        /// Gets or sets текст поста.
+        /// </summary>
+        public string? Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets картики в посте.
+        /// </summary>
+        public string? UrlPicture { get; set; }
+
+        /// <summary>
+        /// Gets or sets видео в посте.
+        /// </summary>
+        public string? UrlVideo { get; set; }
+    }
+}
