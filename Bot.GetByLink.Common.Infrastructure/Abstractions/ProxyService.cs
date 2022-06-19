ba768dd60b7a5401d3467a3c55bd9203e21ba92a
Vladimir Merkel <v.merkel778@gmail.com>
2022-06-19 16:35:09 +0700

    Create abstract class and interface ProxyService

---
+++ Bot.GetByLink.Common.Infrastructure/Abstractions/ProxyService.cs
@@ -0,0 +1,42 @@
+﻿using Bot.GetByLink.Common.Infrastructure.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace Bot.GetByLink.Common.Infrastructure.Abstractions
+{
+    /// <summary>
+    /// Base abstract proxy class.
+    /// </summary>
+    public abstract class ProxyService : IProxyService
+    {
+        /// <summary>
+        /// Gets regex url for proxy.
+        /// </summary>
+        public string[] RegexUrl { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProxyService"/> class.
+        /// </summary>
+        /// <param name="regexUrl">Regex url for proxy.</param>
+        protected ProxyService(string[] regexUrl)
+        {
+            RegexUrl = regexUrl;
+        }
+
+        /// <summary>
+        /// Get match url with regex for this proxy.
+        /// </summary>
+        /// <param name="url">Url for check.</param>
+        /// <returns>Did pass url.</returns>
+        public bool IsMatch(string url)
+        {
+            return RegexUrl.Any(x => Regex.IsMatch(x, url));
+        }
+
+        /// <summary>
+        /// Метод для получения контента поста по объекту апдейта из Telegram.
+        /// </summary>
+        /// <param name="ctx">Url на пост.</param>
+        /// <returns>Объект с текстом и ссылками на картинки и видео присутствующие в посте.</returns>
+        public abstract Task<TelegramMessage> GetContentFromUpdateObject(object? ctx = null);
+    }
+}
---
+++ Bot.GetByLink.Common.Infrastructure/Interfaces/IProxyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.GetByLink.Common.Infrastructure.Interfaces
+{
+    /// <summary>
+    /// Proxy interface.
+    /// </summary>
+    public interface IProxyService
+    {
+        /// <summary>
+        /// Gets regex url for proxy.
+        /// </summary>
+        public string[] RegexUrl { get; }
+
+        /// <summary>
+        /// Get match url with regex for this proxy.
+        /// </summary>
+        /// <param name="url">Url for check.</param>
+        /// <returns>Did pass url.</returns>
+        public bool IsMatch(string url);
+
+        /// <summary>
+        /// Метод для получения контента поста по объекту апдейта из Telegram.
+        /// </summary>
+        /// <param name="ctx">Url на пост.</param>
+        /// <returns>Объект с текстом и ссылками на картинки и видео присутствующие в посте.</returns>
+        public Task<TelegramMessage> GetContentFromUpdateObject(object? ctx = null);
+    }
+}
