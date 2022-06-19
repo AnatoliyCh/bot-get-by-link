b69b48ba2e67ae7b875871b820bf3089660a7967
Vladimir Merkel <v.merkel778@gmail.com>
2022-06-19 17:00:00 +0700

    Update Proxy Reddit
    Create func GetAccessToken and GetContentUrl

--- Bot.GetByLink.Proxy.Reddit/ProxyReddit.cs
+++ Bot.GetByLink.Proxy.Reddit/ProxyReddit.cs
@@ -1,15 +1,18 @@
-﻿using Newtonsoft.Json;
+﻿using Bot.GetByLink.Common.Infrastructure;
+using Bot.GetByLink.Common.Infrastructure.Abstractions;
+using Newtonsoft.Json;
 using Reddit;
 using System.Net.Http.Headers;
 using System.Text;
+using Telegram.Bot.Types;
 using System.Text.RegularExpressions;
 
 namespace Bot.GetByLink.Proxy.Reddit
 {
     /// <summary>
-    /// Апи реддита для получения поста по id.
+    /// Апи реддита для получения контента поста по id или url.
     /// </summary>
-    public class ProxyReddit
+    public class ProxyReddit : ProxyService
     {
         private const string UriString = "www.reddit.com";
         private readonly string appId;
@@ -17,14 +20,27 @@ namespace Bot.GetByLink.Proxy.Reddit
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ProxyReddit"/> class.
-        /// Создание класса Реддита.
         /// </summary>
-        /// <param name="appId">Id приложения реддита.</param>
-        /// <param name="secretId">Secret Id приложения реддита.</param>
-        public ProxyReddit(string appId, string secretId)
+        /// <param name="appId">Id application Reddit.</param>
+        /// <param name="secretId">Secret Id application Reddit.</param>
+        /// <param name="regexUrl">Regex url for proxy.</param>
+        public ProxyReddit(string[] regexUrl, string appId, string secretId)
+            : base(regexUrl)
         {
             this.appId = appId;
             this.secretId = secretId;
+        }
+
+        /// <summary>
+        /// Метод для получения контента поста реддита по url поста.
+        /// </summary>
+        /// <param name="url">Url на пост реддита формата https?://www.reddit.com/r/S+/comments/S+.</param>
+        /// <returns>Объект с текстом и ссылками на картинки и видео присутствующие в посте.</returns>
+        public override async Task<TelegramMessage> GetContentUrl(string url)
+        {
+            var cutUrlPost = url[(url.IndexOf("comments/") + "comments/".Length)..];
+            var postId = cutUrlPost[..cutUrlPost.IndexOf("/")];
+            return await GetContentId(postId);
         }
 
         /// <summary>
@@ -33,14 +49,28 @@ namespace Bot.GetByLink.Proxy.Reddit
         /// <param name="postId">Ид поста.</param>
         /// <returns>Объект с текстом и ссылками на картинки и видео присутствующие в посте.</returns>
         /// <exception cref="ArgumentNullException">Возвращается в случае если был передан пустой postId.</exception>
-        /// <exception cref="HttpRequestException">Возвращается если не получилось авторизоваться в реддите или он вернул пустой ответ.</exception>
-        public TelegramMessage GetPostId(string postId)
+        public async Task<TelegramMessage> GetContentId(string postId)
         {
             if (string.IsNullOrWhiteSpace(postId))
             {
                 throw new ArgumentNullException(nameof(postId), "Пустой id поста");
             }
 
+            var accsessToken = await GetAccessToken();
+            var redditAccess = new RedditClient(appId, appSecret: secretId, accessToken: accsessToken, userAgent: "bot-get-by-link-web");
+            var post = redditAccess.LinkPost($"t3_{postId}").Info();
+            if (post.Listing.Media == null && !post.Listing.IsVideo && !post.Listing.IsRedditMediaDomain) return new TelegramMessage() { Text = $"{post.Listing.URL}\n\n{post.Listing.SelfText}" };
+            if (Regex.IsMatch(post.Listing.URL, @"https?://\S+(?:jpg|jpeg|png)", RegexOptions.IgnoreCase)) return new TelegramMessage() { Text = post.Listing.SelfText, UrlPicture = post.Listing.URL };
+            return new TelegramMessage() { Text = post.Listing.SelfText, UrlVideo = post.Listing.URL };
+        }
+
+        /// <summary>
+        /// Function for get accsess token from reddit.
+        /// </summary>
+        /// <returns>Accsess token reddit.</returns>
+        /// <exception cref="HttpRequestException">Возвращается если не получилось авторизоваться в реддите или он вернул пустой ответ.</exception>
+        private async Task<string> GetAccessToken()
+        {
             var client = new HttpClient();
             client.BaseAddress = new Uri($"https://{UriString}");
             var request = new HttpRequestMessage(HttpMethod.Post, "/api/v1/access_token");
@@ -53,17 +83,13 @@ namespace Bot.GetByLink.Proxy.Reddit
             formData.Add(new KeyValuePair<string, string>("device_id", appId));
 
             request.Content = new FormUrlEncodedContent(formData);
-            var response = client.SendAsync(request).GetAwaiter().GetResult();
+            var response = await client.SendAsync(request);
 
-            var accsessTokenObject = JsonConvert.DeserializeObject<AccessTokenReddit>(response.Content.ReadAsStringAsync().Result);
             response.EnsureSuccessStatusCode();
 
-            if (accsessTokenObject == null) throw new HttpRequestException($"Реддит вернул ошибку: {response.Content.ReadAsStringAsync().Result}");
-            var redditAccess = new RedditClient(appId, appSecret: secretId, accessToken: accsessTokenObject.AccessToken, userAgent: "bot-get-by-link-web");
-            var post = redditAccess.LinkPost($"t3_{postId}").Info();
-            if (post.Listing.Media == null && !post.Listing.IsVideo && !post.Listing.IsRedditMediaDomain) return new TelegramMessage() { Text = $"{post.Listing.URL}\n\n{post.Listing.SelfText}" };
-            if (Regex.IsMatch(post.Listing.URL, @"https?://\S+(?:jpg|jpeg|png)", RegexOptions.IgnoreCase)) return new TelegramMessage() { Text = post.Listing.SelfText, UrlPicture = post.Listing.URL };
-            return new TelegramMessage() { Text = post.Listing.SelfText, UrlVideo = post.Listing.URL };
+            var accsessTokenObject = JsonConvert.DeserializeObject<AccessTokenReddit>(await response.Content.ReadAsStringAsync());
+            if (accsessTokenObject == null || string.IsNullOrWhiteSpace(accsessTokenObject.AccessToken)) throw new HttpRequestException($"Реддит вернул ошибку: {response.Content.ReadAsStringAsync().Result}");
+            return accsessTokenObject.AccessToken;
         }
     }
 }
