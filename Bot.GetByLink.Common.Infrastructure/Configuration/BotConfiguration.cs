d0bc790d7acc65f19e199dc684e72c40b389e169
Anatoly Ch <googlsocial97@gmail.com>
2022-06-26 22:49:07 +0700

    Added types for application configuration

---
+++ Bot.GetByLink.Common.Infrastructure/Configuration/BotConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using Bot.GetByLink.Common.Infrastructure.Interfaces;
+
+namespace Bot.GetByLink.Common.Infrastructure.Configuration;
+
+/// <summary>
+///     Bot сonfiguration class.
+/// </summary>
+public sealed class BotConfiguration : IBotConfiguration
+{
+    /// <summary>
+    ///     Gets name of the project being launched.
+    /// </summary>
+    [JsonPropertyName("ProjectName")]
+    public string ProjectName { get; init; } = string.Empty;
+
+    /// <summary>
+    ///     Gets сollection of client configurations.
+    /// </summary>
+    [JsonPropertyName("Clients")]
+    public ClientsConfiguration Clients { get; init; } = new();
+
+    /// <summary>
+    ///     Gets сollection of proxy configurations.
+    /// </summary>
+    [JsonPropertyName("Proxy")]
+    public ProxyConfiguration Proxy { get; init; } = new();
+}
---
+++ Bot.GetByLink.Common.Infrastructure/Configuration/ProxyConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using Bot.GetByLink.Common.Infrastructure.Configuration.Proxy;
+
+namespace Bot.GetByLink.Common.Infrastructure.Configuration;
+
+/// <summary>
+///     Collection of proxy configurations.
+/// </summary>
+public sealed class ProxyConfiguration
+{
+    /// <summary>
+    ///     Gets Reddit proxy configuration.
+    /// </summary>
+    [JsonPropertyName("Reddit")]
+    public RedditConfiguration Reddit { get; init; } = new();
+
+    /// <summary>
+    ///     Gets Vk proxy configuration.
+    /// </summary>
+    [JsonPropertyName("Vk")]
+    public VkConfiguration Vk { get; init; } = new();
+}
