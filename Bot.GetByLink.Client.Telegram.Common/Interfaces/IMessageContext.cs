272b0be0f0336ccafeb7514718072bb8738f97e9
Anatoly Ch <googlsocial97@gmail.com>
2022-07-11 06:48:58 +0700

    Add IMessageContext

---
+++ Bot.GetByLink.Client.Telegram.Common/Interfaces/IMessageContext.cs
@@ -0,0 +1,12 @@
+﻿using Bot.GetByLink.Common.Infrastructure.Interfaces;
+using Telegram.Bot.Types;
+
+namespace Bot.GetByLink.Client.Telegram.Common.Interfaces;
+
+/// <summary>
+///     The interface of an object that contains all the data to send a message.
+///     TODO: сменить object на тип.
+/// </summary>
+public interface IMessageContext : IMessageContext<ChatId, IEnumerable<string>, object>
+{
+}
---
+++ Bot.GetByLink.Client.Telegram.Common/Model/Message.cs
@@ -0,0 +1,13 @@
+﻿using Bot.GetByLink.Client.Telegram.Common.Interfaces;
+using Telegram.Bot.Types;
+
+namespace Bot.GetByLink.Client.Telegram.Common.Model;
+
+/// <summary>
+///     An object containing all the data to send a message.
+/// </summary>
+/// <param name="ChatId">Сhat ID to send the message.</param>
+/// <param name="Text">Message text.</param>
+/// <param name="Media">Collection of Attached Artifacts.</param>
+public sealed record Message
+    (ChatId ChatId, IEnumerable<string> Text, IEnumerable<object>? Artifacts = null) : IMessageContext;
