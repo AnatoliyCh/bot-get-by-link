65dc76dd4e2215377cfb30d9920838f57a9ac751
Anatoly Ch <googlsocial97@gmail.com>
2022-08-07 21:21:32 +0700

    Init new project Bot.GetByLink.Common.Enums

---
+++ Bot.GetByLink.Common.Enums/Status.cs
@@ -0,0 +1,17 @@
+﻿namespace Bot.GetByLink.Common.Enums;
+
+/// <summary>
+///     Client state types.
+/// </summary>
+public enum Status
+{
+    /// <summary>
+    ///     Represents a running client.
+    /// </summary>
+    On,
+
+    /// <summary>
+    ///     Represents a stopped client.
+    /// </summary>
+    Off
+}
