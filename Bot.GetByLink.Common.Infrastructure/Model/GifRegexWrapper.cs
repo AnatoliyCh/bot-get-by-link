b4b5d55760b6d785bbf52a52114ba59af3466e31
Vladimir Merkel <v.merkel778@gmail.com>
2022-08-20 20:25:51 +0700

    Add gif regex wrapper

---
+++ Bot.GetByLink.Common.Infrastructure/Model/GifRegexWrapper.cs
@@ -0,0 +1,23 @@
+﻿using Bot.GetByLink.Common.Infrastructure.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.GetByLink.Common.Infrastructure.Model
+{
+    /// <summary>
+    ///     Regular expression for gif.
+    /// </summary>
+    public class GifRegexWrapper : RegexWrapper
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GifRegexWrapper" /> class.
+        /// </summary>
+        public GifRegexWrapper()
+            : base(@".(GIF|gif)v?$")
+        {
+        }
+    }
+}
