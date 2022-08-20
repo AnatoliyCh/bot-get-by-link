af06c8e89322286b9c2bb6d600f6ad40c9f3af81
Anatoly Ch <googlsocial97@gmail.com>
2022-08-20 20:22:09 +0700

    Added resource library

---
+++ src/Bot.GetByLink.Common.Resources/ResourceRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Bot.GetByLink.Common.Resources;
+
+/// <summary>
+/// Provides access to text resources.
+/// </summary>
+public static class ResourceRepository
+{
+    /// <summary>
+    /// Returns a text resource by key.
+    /// </summary>
+    /// <param name="key">Resource key.</param>
+    /// <returns>Text resource.</returns>
+    public static string GetClientResource(string key)
+    {
+        switch (key)
+        {
+            case "WrongCommand":
+                return ClientResource.WrongCommand;
+            default:
+                return "-resource empty-";
+        }
+    }
+}
