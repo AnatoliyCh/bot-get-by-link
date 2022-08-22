203c622541913c005faa7bb386b389c4ea3a5875
Anatoly Ch <googlsocial97@gmail.com>
2022-08-22 23:36:13 +0700

    Move Regexs

--- src/Bot.GetByLink.Proxy.Vk/Model/Regexs/WallRegexWrapper.cs
+++ src/Bot.GetByLink.Proxy.Vk/Model/Regexs/WallRegexWrapper.cs
@@ -1,10 +1,13 @@
 ﻿using Bot.GetByLink.Common.Abstractions;
 
-namespace Bot.GetByLink.Proxy.Vk.Regexs;
+namespace Bot.GetByLink.Proxy.Vk.Model.Regexs;
 
 /// <summary>
 ///     Regular expression for Wall VK.
 /// </summary>
+/// https://vk.com/wall-000000000_0
+/// https://vk.com/feed?w=wall-000000000_00
+/// http://vk.com/wall000000000_000
 internal sealed class WallRegexWrapper : RegexWrapper
 {
     /// <summary>
