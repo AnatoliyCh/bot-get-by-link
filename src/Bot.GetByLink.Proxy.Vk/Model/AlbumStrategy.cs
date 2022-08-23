05dd63c79520e2e51b91826b8b427cd747598b3d
Anatoly Ch <googlsocial97@gmail.com>
2022-08-23 23:48:08 +0700

    AlbumStrategy add TryGetByCollectionAsync

--- src/Bot.GetByLink.Proxy.Vk/Model/AlbumStrategy.cs
+++ src/Bot.GetByLink.Proxy.Vk/Model/AlbumStrategy.cs
@@ -54,28 +54,8 @@ public sealed class AlbumStrategy : ContentReturnStrategy
             var (ownerId, albumId) = GetAlbumIds(fullId);
             if (ownerId is null || albumId is null) return null;
 
-            var photos = new List<Photo>((int)step);
-            var offset = 0ul;
-            var photoGetParams = new PhotoGetParams()
-            {
-                OwnerId = ownerId,
-                AlbumId = PhotoAlbumType.Id((long)albumId),
-                Reversed = true,
-                Count = step,
-                Offset = offset,
-            };
             var info = await GetInfobyAlbum((long)ownerId, (long)albumId);
-
-            do
-            {
-                var album = await Api.Photo.GetAsync(photoGetParams);
-                if (album is null || album.Count == 0) break;
-
-                photoGetParams.Offset += step;
-                photos.AddRange(album);
-            }
-            while (true);
-            var media = await photoStrategy.TryGetByCollectionAsync(photos);
+            var media = await GetAlbum((long)ownerId, (long)albumId);
 
             return new ProxyResponseContent(info, UrlPicture: media);
         }
@@ -92,9 +72,29 @@ public sealed class AlbumStrategy : ContentReturnStrategy
     /// <typeparam name="T">Collection Item Type.</typeparam>
     /// <param name="collection">Photo collection.</param>
     /// <returns>A collection of information objects about attached Photos.</returns>
-    public override Task<IEnumerable<IMediaInfo>?> TryGetByCollectionAsync<T>(IEnumerable<T> collection)
+    public override async Task<IEnumerable<IMediaInfo>?> TryGetByCollectionAsync<T>(IEnumerable<T> collection)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (collection is not IEnumerable<Album> albums || !collection.Any()) return null;
+
+            IEnumerable<IMediaInfo> medias = new List<MediaInfo>((int)step);
+            var mediasAsList = medias.ToList();
+            foreach (var album in albums)
+            {
+                if (album.OwnerId is null || album.Thumb.AlbumId is null) continue;
+
+                var photos = await GetAlbum((long)album.OwnerId, (long)album.Thumb.AlbumId);
+                if (photos is not null && photos.Any()) mediasAsList.AddRange(photos);
+            }
+
+            return mediasAsList.Where(item => item is not null);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "Proxy VK : AlbumStrategy : TryGetByCollectionAsync");
+            return null;
+        }
     }
 
     private static (long? OwnerId, long? AlbumId) GetAlbumIds(string fullId)
@@ -118,5 +118,30 @@ public sealed class AlbumStrategy : ContentReturnStrategy
         if (album is null) return string.Empty;
 
         return $"{album.Title}\n{album.Description}";
+    }
+
+    private async Task<IEnumerable<IMediaInfo>?> GetAlbum(long ownerId, long albumId)
+    {
+        var photos = new List<Photo>((int)step);
+        var offset = 0ul;
+        var photoGetParams = new PhotoGetParams()
+        {
+            OwnerId = ownerId,
+            AlbumId = PhotoAlbumType.Id(albumId),
+            Reversed = true,
+            Count = step,
+            Offset = offset,
+        };
+        do
+        {
+            var album = await Api.Photo.GetAsync(photoGetParams);
+            if (album is null || album.Count == 0) break;
+
+            photoGetParams.Offset += step;
+            photos.AddRange(album);
+        }
+        while (true);
+
+        return await photoStrategy.TryGetByCollectionAsync(photos);
     }
 }
