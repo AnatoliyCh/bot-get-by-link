05414393f4968cd58d92ca097193de0d73fa766b
Vladimir Merkel <v.merkel778@gmail.com>
2022-08-27 19:55:45 +0700

    Update description in help command

--- src/Bot.GetByLink.Client.Telegram.Common/Model/Commands/HelpCommand.cs
+++ src/Bot.GetByLink.Client.Telegram.Common/Model/Commands/HelpCommand.cs
@@ -14,15 +14,19 @@ namespace Bot.GetByLink.Client.Telegram.Common.Model.Commands;
 public sealed class HelpCommand : AsyncCommand<CommandName>
 {
     private readonly IAsyncCommand<CommandName> sendMessageCommand;
+    private readonly int delaySendingMediaGroupSeconds;
 
     /// <summary>
     ///     Initializes a new instance of the <see cref="HelpCommand" /> class.
     /// </summary>
     /// <param name="sendMessageCommand">Sends a message to the client.</param>
-    public HelpCommand(IAsyncCommand<CommandName> sendMessageCommand)
+    /// <param name="delaySendingMediaGroupMilliseconds">Interval in milliseconds between sent messages</param>
+    /// <exception cref="ArgumentNullException">Any argument is null.</exception>
+    public HelpCommand(IAsyncCommand<CommandName> sendMessageCommand, int delaySendingMediaGroupMilliseconds)
         : base(CommandName.Help)
     {
         this.sendMessageCommand = sendMessageCommand ?? throw new ArgumentNullException(nameof(sendMessageCommand));
+        delaySendingMediaGroupSeconds = delaySendingMediaGroupMilliseconds > 0 ? delaySendingMediaGroupMilliseconds / 1000 : 0;
     }
 
     /// <summary>
@@ -37,7 +41,7 @@ public sealed class HelpCommand : AsyncCommand<CommandName>
         var chatId = update.Message?.Chat.Id;
         if (chatId is null) return;
 
-        var message = new Message(chatId, new[] { ResourceRepository.GetClientResource(ClientResource.HelpCommand) },
+        var message = new Message(chatId, new[] { string.Format(ResourceRepository.GetClientResource(ClientResource.HelpCommand), delaySendingMediaGroupSeconds) },
             ParseMode: ParseMode.Markdown);
         await sendMessageCommand.ExecuteAsync(message);
     }
