e88264fb68dfd99afaa12694332e2741a7e0dc16
Vladimir Merkel <v.merkel778@gmail.com>
2022-09-07 19:03:24 +0700

    Fix reddit regex wrapper

--- src/Bot.GetByLink.Proxy.Reddit/Regexs/RedditPostRegexWrapper.cs
+++ src/Bot.GetByLink.Proxy.Reddit/Regexs/RedditPostRegexWrapper.cs
@@ -5,14 +5,14 @@ namespace Bot.GetByLink.Proxy.Reddit.Regexs;
 /// <summary>
 ///     Regular expression for Reddit main comment.
 /// </summary>
-/// https://www.reddit.com/r/*/comments/*
-internal sealed class RedditPostRegexWrapper : RegexWrapper
+/// https://www.reddit.com/r/*/(comments|gallery)/*
+public sealed class RedditPostRegexWrapper : RegexWrapper
 {
     /// <summary>
     ///     Initializes a new instance of the <see cref="RedditPostRegexWrapper" /> class.
     /// </summary>
     public RedditPostRegexWrapper()
-        : base(@"https?:\/\/www.reddit.com\/r\/\S+/comments\/\S+")
+        : base(@"https?:\/\/www.reddit.com[\/]?([\S]+?)[\/]?(comments|gallery)\/\S+")
     {
     }
 }
