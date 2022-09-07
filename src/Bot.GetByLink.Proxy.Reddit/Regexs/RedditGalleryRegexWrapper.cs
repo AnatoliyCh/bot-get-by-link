a57279a17df8623745afe88a9cb9b0b8246d68a0
Vladimir Merkel <v.merkel778@gmail.com>
2022-09-07 19:03:09 +0700

    Add regex gallery reddit

---
+++ src/Bot.GetByLink.Proxy.Reddit/Regexs/RedditGalleryRegexWrapper.cs
@@ -0,0 +1,24 @@
+﻿using Bot.GetByLink.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.GetByLink.Proxy.Reddit.Regexs
+{
+    /// <summary>
+    ///     Regular expression for Reddit main comment.
+    /// </summary>
+    /// https://www.reddit.com/r/*/gallery/*
+    public sealed class RedditGalleryRegexWrapper : RegexWrapper
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RedditGalleryRegexWrapper" /> class.
+        /// </summary>
+        public RedditGalleryRegexWrapper()
+            : base(@"https?:\/\/www.reddit.com\/r\/\S+/gallery\/\S+")
+        {
+        }
+    }
+}
