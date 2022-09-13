150255067abae6dd2bcac5177865ad96ecb82e8d
Vladimir Merkel <v.merkel778@gmail.com>
2022-09-13 18:26:54 +0700

    Add class for redgif json

---
+++ src/Bot.GetByLink.Proxy.Reddit/Model/RedGif/RedgifData.cs
@@ -0,0 +1,24 @@
+﻿using Bot.GetByLink.Proxy.Reddit.Model.GfyCat;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Bot.GetByLink.Proxy.Reddit.Model.RedGif
+{
+    /// <summary>
+    ///     Class for redgif data.
+    /// </summary>
+    public sealed class RedGifData
+    {
+        /// <summary>
+        ///     Gets redgif gif.
+        /// </summary>
+        [JsonPropertyName("gif")]
+        public RedGifItem? Gif { get; init; } = null;
+
+        /// <summary>
+        ///     Gets unknow fields.
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, JsonElement>? UnknowFields { get; init; } = null;
+    }
+}
