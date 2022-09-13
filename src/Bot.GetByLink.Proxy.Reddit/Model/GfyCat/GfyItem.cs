30cc69391b5ba2aef6b7b92ecd47e9bd87670e25
Vladimir Merkel <v.merkel778@gmail.com>
2022-09-13 18:27:16 +0700

    Add class for gfycat json

---
+++ src/Bot.GetByLink.Proxy.Reddit/Model/GfyCat/GfyItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Bot.GetByLink.Proxy.Reddit.Model.GfyCat
+{
+    /// <summary>
+    ///     Class for gfycat data item.
+    /// </summary>
+    public sealed class GfyItem
+    {
+        /// <summary>
+        ///     Gets item content.
+        /// </summary>
+        [JsonPropertyName("content_urls")]
+        public IDictionary<string, GfyCatItemContent>? ContentUrls { get; init; } = null;
+
+        /// <summary>
+        ///     Gets a value indicating whether item has audio.
+        /// </summary>
+        [JsonPropertyName("hasAudio")]
+        public bool HasAudio { get; init; } = false;
+
+        /// <summary>
+        ///     Gets unknow fields.
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, JsonElement>? UnknowFields { get; init; } = null;
+    }
+}
---
+++ src/Bot.GetByLink.Proxy.Reddit/Model/GfyCat/GfycatData.cs
@@ -0,0 +1,24 @@
+﻿using Bot.GetByLink.Proxy.Reddit.Model.Reddit;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Bot.GetByLink.Proxy.Reddit.Model.GfyCat
+{
+    /// <summary>
+    ///     Class for gfycat data api.
+    /// </summary>
+    public sealed class GfycatData
+    {
+        /// <summary>
+        ///     Gets gfy item.
+        /// </summary>
+        [JsonPropertyName("gfyItem")]
+        public GfyItem? GfyItem { get; init; } = null;
+
+        /// <summary>
+        ///     Gets unknow fields.
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, JsonElement>? UnknowFields { get; init; } = null;
+    }
+}
