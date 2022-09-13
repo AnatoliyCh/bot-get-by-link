df6f00800525def948122cd9512bcf3c58f2f341
Vladimir Merkel <v.merkel778@gmail.com>
2022-09-13 18:25:37 +0700

    Add new func in ProxyHelper

--- src/Bot.GetByLink.Proxy.Common/ProxyHelper.cs
+++ src/Bot.GetByLink.Proxy.Common/ProxyHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace Bot.GetByLink.Proxy.Common;
+﻿using System.Net.Http.Headers;
+
+namespace Bot.GetByLink.Proxy.Common;
 
 /// <summary>
 ///     Class general methods for proxy.
@@ -22,8 +24,31 @@ public static class ProxyHelper
         var message = await client.SendAsync(request);
         if (long.TryParse(
                 message.Content.Headers.FirstOrDefault(h => h.Key.Equals("Content-Length")).Value?.FirstOrDefault(),
-                out var contentLength)) result = contentLength == 0 ? contentLength : contentLength / OneMb;
+                out var contentLength)) result = contentLength == 0 ? contentLength : GetMbFromByte(contentLength);
 
         return result;
+    }
+
+    /// <summary>
+    /// Function for cast byte to megabyte.
+    /// </summary>
+    /// <param name="countByte">Count byte.</param>
+    /// <returns>Count megabyte.</returns>
+    public static long GetMbFromByte(long countByte) => countByte / OneMb;
+
+    /// <summary>
+    ///     Function for check has content from url.
+    /// </summary>
+    /// <param name="url">Url.</param>
+    /// <returns>Has url content.</returns>
+    public static async Task<bool> CheckContentUrl(string url)
+    {
+        HttpClient client = new();
+        client.BaseAddress = new Uri(url);
+        var productValue = new ProductInfoHeaderValue("BotGetByLink", "1.0");
+        client.DefaultRequestHeaders.UserAgent.Add(productValue);
+        var request = new HttpRequestMessage(HttpMethod.Head, string.Empty);
+        var response = await client.SendAsync(request);
+        return response.IsSuccessStatusCode;
     }
 }
